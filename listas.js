a4d869e6385f8aef0b3defb6b58b81203d61fa93
cirdanalcarin <cirdanalcarin13@gmail.com>
2018-11-01 14:44:37 +0100

    list v2

--- listas.js
+++ listas.js
@@ -25,10 +25,56 @@ function isFull(list) {
     }
 }
 
+function size(list) {
+    var counter = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (!isNaN(list[i])) {
+            counter++;
+        }
+    }
+    return counter;
+}
 
+function add(list, elem) {
+    try {
+        if (isFull(list)) {
+            throw "Full list";
+        } else if (!isNaN(elem)) {
+            list[size(list)] = elem;
+        }
+        return size(list);
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
 
+function addAt(list, elem, index) {
+    try {
+        if (isFull(list)) {
+            throw "Full list";
+        } else if (!isNaN(elem) && (index >= 0 && index < SIZE)) {
+            list[index] = elem;
+        }
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
+
+function get(list, index) {
+    var elem = 0;
+    try {
+        if (isEmpty(list) && (index >= 0 && index < SIZE)) {
+            throw "Empty list, you can not get the item";
+        } else {
+            elem = list[index];
+        }
+        return elem;
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
 var list = create();
 console.log(list);
 console.log(isEmpty(list));
 console.log(isFull(list));
-console.log(list);
+console.log(size(list));
